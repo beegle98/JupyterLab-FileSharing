1a6b276b6634cb47f6b82a329c87b71e968cc89f
beegle98 <ckwjdans@ajou.ac.kr>
2021-10-08 13:45:25 +0900

    add inputdialog

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {
   JupyterFrontEndPlugin,
 } from '@jupyterlab/application';
 import { IFileBrowserFactory } from '@jupyterlab/filebrowser';
-import { showDialog, Dialog } from '@jupyterlab/apputils';
+import { showDialog, Dialog, InputDialog } from '@jupyterlab/apputils';
 import { buildIcon } from '@jupyterlab/ui-components';
 
 const extension: JupyterFrontEndPlugin<void> = {
@@ -13,13 +13,35 @@ const extension: JupyterFrontEndPlugin<void> = {
   activate: (app: JupyterFrontEnd, factory: IFileBrowserFactory) => {
     app.commands.addCommand('context-menu:open', {
       label: 'Share files',
-      caption: "Example context menu button for file browser's items.",
+      caption: "context menu button for file browser's items.",
       icon: buildIcon,
       execute: () => {
-        factory.tracker.currentWidget!.selectedItems
         const file = factory.tracker.currentWidget?.selectedItems().next();
+        const options = ['one', 'two', 'three']
 
+        InputDialog.getText({
+          title : 'example',
+
+        }).then(result =>{
+          if(result.button.accept){
+            console.log(result.value);
+          }
+        })
+        
+        InputDialog.getItem({
+          title : 'example',
+          items : options,
+          current : Math.max(0, options.indexOf('one')),
+
+        }).then((result) => {
+          // If the user click on the accept button of the dialog
+          if (result.button.accept) {
+            console.log(result.value);            
+            
+          }
+        })
         showDialog({
+          
           title: 'Share file : ' + file?.name, 
           body: 'Share file with professor : '+ file?.path,
           buttons: [
@@ -31,7 +53,7 @@ const extension: JupyterFrontEndPlugin<void> = {
           if(result.button.accept){
             console.log(file?.name + " Share Success ");
           }
-        });
+        }).catch((e) => console.log(e));
         
       },
     });
