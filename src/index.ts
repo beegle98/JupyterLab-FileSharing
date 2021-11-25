eb787cd905dd8b50487a6b72095679ea3df1d09d
beegle98 <ckwjdans@ajou.ac.kr>
2021-11-25 18:15:50 +0900

    add uploadURL and get file as a blob

--- src/index.ts
+++ src/index.ts
@@ -12,34 +12,7 @@ const extension: JupyterFrontEndPlugin<void> = {
   requires: [IFileBrowserFactory],
   
   activate: (app: JupyterFrontEnd, factory: IFileBrowserFactory) => {
-    app.commands.addCommand('FileSharing/context-menu:open', {
-      
-     
-      /*id select 필요없을 예정
-        const options = ['one', 'two', 'three']
-        InputDialog.getItem({
-          title : 'example',
-          items : options,
-          current : Math.max(0, options.indexOf('one')),
-
-        }).then((result) => {
-          // If the user click on the accept button of the dialog
-          if (result.button.accept) {
-            console.log(result.value);            
-            
-          }
-        })*/
-        /*
-            fetch('url',{
-              method : 'post',
-              headers : {'Content-Type' : 'application/json'},
-              body : JSON.stringify({
-                // JSON 형식으로 데이터를 전송
-              })
-            }).then(res => res.json())
-          */
-      
-      
+    app.commands.addCommand('FileSharing/context-menu:open', {          
       label: 'Share file',
       caption: "context menu button for file browser's items.",
       icon: buildIcon,
@@ -49,18 +22,14 @@ const extension: JupyterFrontEndPlugin<void> = {
         const widget = factory.tracker.currentWidget;
         const file = widget.selectedItems().next();
         
-        console.log('file: ' + file.path);
-
+        let input_url : string;
         
-        var input_url : any;
-
         InputDialog.getText({
           title : 'Please enter the URL',
           
         }).then(result =>{
           if(result.button.accept){
             input_url = result.value
-            console.log("inputURL : " + input_url);
 
             showDialog({
           
@@ -73,26 +42,41 @@ const extension: JupyterFrontEndPlugin<void> = {
               
             }).then(result => {
               if(result.button.accept){
+
                 console.log( "URL : " + input_url +"\n"+ "Shared the file :" + file.name );
-                let send_file = new FormData();
-                
-                
-                send_file.append('file',
-                new Blob([file.mimetype], {type: 'multipart/form-data'})
-                ,file.name);
-                send_file.append('url',
-                new Blob([JSON.stringify({"url": input_url})], { type: 'application/json' })
-                );
+
+
+                fetch('http://localhost:3000/uploadURL',{
+                  method : 'post',
+                  headers : {'Content-Type' : 'application/json'},                 
+                  body : JSON.stringify({
+                    fileName : file.name,
+                    url : input_url
+                  })
+                }).then(res => res.json())
+                .then(res => console.log(res))
 
                 
-                fetch('http://localhost:3000/file/upload',{
+                let uploadFile = new FormData();
+
+                console.log('http://localhost:8888/lab/tree/' + file.path);
+                fetch('http://localhost:8888/files/' + file.path +'?_xsrf=2%7C6cc81390%7C5bb85f06295e2c5df9b85c1ac96ce502%7C1635325547')
+                .then(res => res.blob())
+                .then(function(myblob){
+                  console.log("Size: " + myblob.size);
+                  console.log("type: \n" + myblob.type);
+                  uploadFile.append('File',myblob, file.name);
+                  
+                fetch('http://localhost:3000/uploadFile',{
                   method : 'post',
-                  headers : {'Content-Type': 'application/x-www-form-urlencoded'},
-                  body : send_file
+                  headers : {},
+                  body : uploadFile
     
                 }).then(res => res.json())
                 .then(res => console.log(res))
                 .catch(e => console.log(e))
+                });
+
               }
             }).catch((e) => console.log(e));
           }
